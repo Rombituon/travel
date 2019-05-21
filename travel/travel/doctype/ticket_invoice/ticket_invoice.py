621ba070687cadb3c3aaedf797a0b33e74d6699a
Bilal Ghayad <bilal@ghayad.com>
2019-05-21 03:06:17 +0300

    Ticket Invoice is integrated with GL and Payment Entry, also there is validation on ticket no. Still the following is not finished: Internal payment, payment terms, commission and contribution

--- travel/travel/doctype/ticket_invoice/ticket_invoice.py
+++ travel/travel/doctype/ticket_invoice/ticket_invoice.py
@@ -4,15 +4,39 @@
 
 from __future__ import unicode_literals
 import frappe
+import string, random, re
 from frappe.model.document import Document
 from erpnext.accounts.general_ledger import make_gl_entries
 from erpnext.accounts.general_ledger import delete_gl_entries
 from erpnext.controllers.accounts_controller import AccountsController
-from frappe import utils
+from frappe import utils, _
 
 #class TicketInvoice(Document):
 class TicketInvoice(AccountsController):
 #	pass
+
+	def validate(self):
+		i = 0
+		for d in self.items:
+			j = i + 1
+			i = i + 1
+			while j < len(self.items):
+				if d.ticket_no == self.items[j].get('ticket_no'):
+					frappe.msgprint("Ticket No {0} is duplicated with row number {1}". format(d.ticket_no, j+1));
+				j = j+1
+				
+			parent = frappe.db.get_value("Ticket Invoice Ticket",{"ticket_no": d.ticket_no, "docstatus": ["!=", 2], "parent": ["!=", self.name]}, "parent")
+			if parent:
+				frappe.throw(_("Duplicated Ticket No is not allowed. Ticket No {0} is already existed in Ticket Invoice No {1}"). 
+						format(d.ticket_no, parent))
+			if d.ticket_no:
+				carrier_no = frappe.db.get_value("Carrier Settings",{"carrier_no": d.ticket_no.split('-')[0]}, "carrier_no")
+				if not carrier_no:
+					frappe.throw(_("Carrier No {0} is not existed for any carrier"). format(d.ticket_no.split('-')[0]))
+			elif not d.ticket_no:
+				frappe.throw(_("Ticket No should not be empty. Please check ticket no at row # {0}"). format(i))
+				
+
 	def on_cancel(self):
 		delete_gl_entries(voucher_type=self.doctype, voucher_no=self.name)
 
